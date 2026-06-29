f19694493f2592149a4e7e757b5b3c7dfad7efa8
danielsuen523 <danielsuen523@gmail.com>
2026-06-29 23:07:26 +0800

    add DataTable and columns definitions for 講道錄音 page

---
+++ app/worship/sermons/columns.tsx
@@ -0,0 +1,99 @@
+// this file contains the column definitions
+
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { MoreHorizontal } from "lucide-react"
+import { ArrowUpDown } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Payment = {
+  date: string
+  speaker: string
+  type: "lunch" | "third" | "christmas"
+  topic: string
+  verse: string
+  powerpoint: string
+  mp3: string
+}
+
+export const columns: ColumnDef<Payment>[] = [
+    {
+    accessorKey: "date",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          主日
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  {
+    accessorKey: "speaker",
+    header: () => <div className="">講員</div>,
+    /*cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"))
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount)
+
+      return <div className="text-right font-medium">{formatted}</div>
+    },*/
+  },
+  {
+    accessorKey: "verse",
+    header: () => <div className="">經文</div>,
+  },
+  {
+    accessorKey: "powerpoint",
+    header: () => <div className="">投影片</div>,
+  },
+  {
+    accessorKey: "mp3",
+    header: () => <div className="">MP3</div>,
+  },
+  /*{
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original
+ 
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.date)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  },*/
+]
--- app/worship/sermons/page.tsx
+++ app/worship/sermons/page.tsx
@@ -1,10 +1,43 @@
-export default function TimetablePage() {
+export default async function TimetablePage() {
+  
+  const data = await getData()
+  
   return (
     <div className="container mx-auto py-10 px-4">
       <h1 className="text-3xl font-bold tracking-tight mb-4">講道錄音</h1>
-      <p className="text-muted-foreground text-lg">
+      {/* <p className="text-muted-foreground text-lg">
         Coming Soon!
-      </p>
+      </p> */}
+
+      <DataTable columns={columns} data={data} />
     </div>
   )
+}
+
+import { columns, Payment } from "./columns"
+import { DataTable } from "./data-table"
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      date: "2025-10-26",
+      speaker: "黃天相博士",
+      type: "lunch",
+      topic: "事奉者的約",
+      verse: "瑪拉基書2章1-9節",
+      powerpoint: "N/A",
+      mp3: "因應講員要求而未能提供錄音，請見諒！"
+    },
+    {
+      date: "2025-10-19",
+      speaker: "關卓凌傳道",
+      type: "third",
+      topic: "若然未報，時辰未到？",
+      verse: "路加福音 18章1-8節",
+      powerpoint: "251019",
+      mp3: "251019",
+    },
+    // ...
+  ]
 }
