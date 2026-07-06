47f3bb12779dd2747027d0dc699fe88effbae329
danielsuen523 <danielsuen523@gmail.com>
2026-07-06 23:25:30 +0800

    update max-width for header for improved responsiveness

--- components/header.tsx
+++ components/header.tsx
@@ -84,7 +84,7 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
-      <div className="flex h-20 items-center justify-between px-4">
+      <div className="mx-auto flex h-20 w-full max-w-400 items-center justify-between px-4">
         {/* logo */}
         <Link
           href="/"
